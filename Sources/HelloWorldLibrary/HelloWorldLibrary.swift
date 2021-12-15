9bc08f1c3884e5cf7e9ed8e561230dcf94af43f3
Vedant Jha <6vedant@gmail.com>
2021-12-15 16:36:49 +0530

    updates
    updates

--- Sources/HelloWorldLibrary/HelloWorldLibrary.swift
+++ Sources/HelloWorldLibrary/HelloWorldLibrary.swift
@@ -2,7 +2,7 @@ public struct HelloWorldLibrary {
     public private(set) var text = "Hello, World!"
 
     public init() {
-        print(getHello())
+        
     }
     public func getHello() -> String {
         return self.text
